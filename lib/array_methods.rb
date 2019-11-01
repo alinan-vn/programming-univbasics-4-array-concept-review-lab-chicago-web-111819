14b9173bb3dca97e7dae5997d9beedfd017b37c9
Alejandro Linan <jandrolinan@gmail.com>
2019-11-01 15:16:05 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,9 +4,11 @@ end
 
 def find_max_value(array)
   array.sort 
-  
+  num = array.length - 1
+  return array[num]
 end
 
 def find_min_value(array)
-  # Add your solution here
+  array.sort
+  return array[0]
 end
